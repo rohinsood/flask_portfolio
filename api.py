9be4cb4580c6b62a39b4156355bd57773d18cf46
muddydog25 <etran8@gmail.com>
2022-11-02 14:33:56 -0700

    fixed api routing

--- api.py
+++ api.py
@@ -1,33 +1,92 @@
-from base64 import encode
+import random
+from flask import request
+import uuid
+import requests
 from flask import Blueprint , jsonify
+from base64 import encode
 
 api_bp = Blueprint(
-    "api" , 
+    "nba" , 
     __name__ , 
-    url_prefix='/api',
+    url_prefix='/nba',
     static_folder='static',
-    static_url_path='static/api'    
+    static_url_path='static/nba'    
 )
 
-@api_bp.route('/decode/<code>')
-def decode(code):
-    if len(code) != 20 or not code.isnumeric():
-        return "invalid code"
+url = "https://free-nba.p.rapidapi.com/teams"
 
-    save_dict = dict()
+headers = {
+	"X-RapidAPI-Key": "9275b62a1fmsh3b832340dafb492p1abc77jsn58ef554feee6",
+	"X-RapidAPI-Host": "free-nba.p.rapidapi.com"
+}
 
-    ore = int(code[:10].lstrip("0"))
-    upgrade_1 = int(code[10:12].lstrip("0"))
-    upgrade_2 = int(code[12:14].lstrip("0"))
-    upgrade_3 = int(code[14:16].lstrip("0"))
-    upgrade_4 = int(code[16:18].lstrip("0"))
-    upgrade_5 = int(code[18:20].lstrip("0"))
+response = requests.request("GET", url, headers=headers)
 
-    save_dict["ore"] = ore
-    save_dict["upgrade_one"] = upgrade_1
-    save_dict["upgrade_two"] = upgrade_2
-    save_dict["upgrade_three"] = upgrade_3
-    save_dict["upgrade_four"] = upgrade_4
-    save_dict["upgrade_five"] = upgrade_5
+response.raise_for_status()
+if response.status_code != 204:
+    teams_json = response.json()['data']
 
-    return jsonify(save_dict)
+teams_dict = {}
+
+logos = [
+    'https://cdn.discordapp.com/attachments/902298316094717963/1036481642979663973/hawks.png',
+    'https://cdn.discordapp.com/attachments/902298316094717963/1036481671505137684/celtic.png',
+    'https://cdn.discordapp.com/attachments/902298316094717963/1036481746075656302/nets.png',
+    'https://cdn.discordapp.com/attachments/902298316094717963/1036481787343409272/hornets.png',
+    'https://media.discordapp.net/attachments/902298316094717963/1036481822856577035/bulls.png',
+    'https://cdn.discordapp.com/attachments/902298316094717963/1036481856662667344/cavaliers.png',
+    'https://cdn.discordapp.com/attachments/902298316094717963/1036481903387217940/mavs.png',
+    'https://cdn.discordapp.com/attachments/902298316094717963/1036482021691768883/nuggets.png',
+    'https://cdn.discordapp.com/attachments/902298316094717963/1036482143838273607/pistons.png',
+    'https://cdn.discordapp.com/attachments/902298316094717963/1036482569593692170/image-removebg-preview_1.png',
+    'https://cdn.discordapp.com/attachments/902298316094717963/1036482827660828712/rockets.png',
+    'https://cdn.discordapp.com/attachments/902298316094717963/1036483337570750475/image-removebg-preview_2.png',
+    'https://cdn.discordapp.com/attachments/902298316094717963/1036483452196892682/clippers.png',
+    'https://cdn.discordapp.com/attachments/902298316094717963/1036484104180465774/image-removebg-preview_3.png',
+    'https://cdn.discordapp.com/attachments/902298316094717963/1036484246862319626/image-removebg-preview_4.png',
+    'https://cdn.discordapp.com/attachments/902298316094717963/1036484797444399124/image-removebg-preview_5.png',
+    'https://cdn.discordapp.com/attachments/902298316094717963/1036485074893414420/image-removebg-preview_6.png',
+    'https://cdn.discordapp.com/attachments/902298316094717963/1036485189418889257/twolves.png',
+    'https://cdn.discordapp.com/attachments/902298316094717963/1036485551278260324/unknown.png',
+    'https://media.discordapp.net/attachments/902298316094717963/1036486385927983134/knicks.png?width=827&height=671',
+    'https://cdn.discordapp.com/attachments/902298316094717963/1036486892918689823/image-removebg-preview_7.png',
+    'https://media.discordapp.net/attachments/902298316094717963/1036487041409613874/magic.png?width=671&height=671',
+    'https://cdn.discordapp.com/attachments/902298316094717963/1036493788971028510/sixers.png',
+    'https://cdn.discordapp.com/attachments/902298316094717963/1036493947507331142/unknown.png',
+    'https://cdn.discordapp.com/attachments/902298316094717963/1036494365134180382/image-removebg-preview_8.png',
+    'https://cdn.discordapp.com/attachments/902298316094717963/1036494443907395595/kings.png',
+    'https://cdn.discordapp.com/attachments/902298316094717963/1036494609121034302/spurs.png',
+    'https://cdn.discordapp.com/attachments/902298316094717963/1036494778323456030/image-removebg-preview_9.png',
+    'https://cdn.discordapp.com/attachments/902298316094717963/1036495011992309820/jazz.webp',
+    'https://media.discordapp.net/attachments/902298316094717963/1036495113607729192/wizards.png?width=671&height=671',
+]
+
+for i, item in enumerate(teams_json):
+    teams_dict[i] = {"likes": 0, "dislikes": 0, "name": item['full_name'].split()[-1],
+                     "abbreviation": item['abbreviation'], "division": item["division"], "city": item['city'], "logo": logos[i]}
+
+
+@api_bp.route('/')
+def index():
+    return "TEAM BRO BRO YOU ALREADY KNOW KNOW WE ARE SO FLY WE ALWAYS GO GO"
+
+
+@api_bp.route('/like', methods=["POST"])
+def like():
+    data = request.json
+    id = data["id"]
+    teams_dict[id]["likes"] += 1
+    return {"likes": teams_dict[id]["likes"]}
+
+
+@api_bp.route('/dislike', methods=["POST"])
+def dislike():
+    data = request.json
+    id = data["id"]
+    teams_dict[id]["dislikes"] += 1
+    return {"dislikes": teams_dict[id]["dislikes"]}
+
+
+@api_bp.route('/teams', methods=["GET"])
+def teams():
+    return teams_dict
